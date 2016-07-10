e72f127189ff7b6ca26a874491b643b2009e13eb
enleightond <enleightond@yahoo.com>
2016-07-10 15:05:36 -0700

    added refactor for perfect square

--- perfectSqr.js
+++ perfectSqr.js
@@ -1,7 +1,7 @@
 // brute force approach
 function pSqr (num) {
 	// edge case: if num is 1, 0, or a negitive number
-	if num <= 1 {
+	if (num <= 1) {
 		return true;
 	}
 	// solve with simple loop 
@@ -14,6 +14,24 @@ function pSqr (num) {
 
 pSqr(49);  // true
 
+// refactor to address big O time issue
+function pSqrRe(num){
+	// edge case: if num is 1, 0, or a negitive number
+	if (num <= 1) {
+		return true;
+	}
+	var newNum = 0;
+	// solve with sum
+	for (var i = 1; i < num/2; i++) {
+		if (i % 2 !== 0) {
+			newNum += i;
+		}
+		if (newNum === num) {
+		return true;
+		}
+	} 	
+}
 
+pSqrRe(81);
 
 
